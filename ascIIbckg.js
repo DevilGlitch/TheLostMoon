7a5c6f114bbfd9b6a5d87503d8e6031424c3a533
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2023-04-30 13:03:16 -0700

    testing

---
+++ ascIIbckg.js
@@ -0,0 +1,9 @@
+class ascIIbckg { 
+    width = screen.width;
+    height = screen.height;
+    timer = 0;
+    black = '&blk34';       // ▓
+    grey = '&blk12;';       //  ▒
+    white = '&blk14;';      //   ░
+    bckg = {}; 
+}
