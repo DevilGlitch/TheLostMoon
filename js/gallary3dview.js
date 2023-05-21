e41668f99f6a2519f7ed701fce9a1bc39a6485e6
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2023-05-21 12:42:22 -0700

    fix?

--- js/gallary3dview.js
+++ js/gallary3dview.js
@@ -1,49 +1,25 @@
-
-var carousel = document.querySelector('.carousel');
-var carouselInner = carousel.querySelector('.carousel-inner');
-var carouselItems = carousel.querySelectorAll('.carousel-item');
-var totalItems = carouselItems.length;
-var currentIdx = 0;
-
-function showNext() {
-  var nextIdx = (currentIdx + 1) % totalItems;
+window.addEventListener('DOMContentLoaded', function() {
+    var carouselItems = document.querySelectorAll('.carousel-item');
+    var currentIndex = 0;
+    var timer = setInterval(nextSlide, 3000); // Switch slide every 3 seconds
   
-  carouselItems[currentIdx].classList.remove('active');
-  carouselItems[nextIdx].classList.add('active');
+    function nextSlide() {
+      carouselItems[currentIndex].classList.remove('active');
+      currentIndex = (currentIndex + 1) % carouselItems.length;
+      carouselItems[currentIndex].classList.add('active');
+    }
   
-  carouselItems[currentIdx].classList.add('prev');
-  carouselItems[nextIdx].classList.add('next');
+    function stopTimer() {
+      clearInterval(timer);
+    }
   
-  setTimeout(function() {
-    carouselItems[currentIdx].classList.remove('prev');
-    carouselItems[nextIdx].classList.remove('next');
-  }, 600);
+    function startTimer() {
+      timer = setInterval(nextSlide, 3000);
+    }
   
-  currentIdx = nextIdx;
-}
-function showPrevious() {
-  var prevIdx = (currentIdx - 1 + totalItems) % totalItems;
+    // Pause timer on mouseenter, resume on mouseleave
+    var carousel = document.querySelector('.carousel');
+    carousel.addEventListener('mouseenter', stopTimer);
+    carousel.addEventListener('mouseleave', startTimer);
+  });
   
-  carouselItems[currentIdx].classList.remove('active');
-  carouselItems[prevIdx].classList.add('active');
-  
-  carouselItems[currentIdx].classList.add('next');
-  carouselItems[prevIdx].classList.add('prev');
-  
-  setTimeout(function() {
-    carouselItems[currentIdx].classList.remove('next');
-    carouselItems[prevIdx].classList.remove('prev');
-  }, 600);
-  
-  currentIdx = prevIdx;
-}
-
-carousel.addEventListener('click', function(e) {
-  var isNext = e.clientX > carousel.getBoundingClientRect().width / 2;
-  
-  if (isNext) {
-    showNext();
-  } else {
-    showPrevious();
-  }
-});
