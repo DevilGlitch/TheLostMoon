bd3d9f47d19b07b0a8c61945c29951dc4b861b71
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2023-05-21 11:00:53 -0700

    changes to visuals

--- ascIIbckg.js
+++
@@ -1,9 +0,0 @@
-class ascIIbckg { 
-    width = screen.width;
-    height = screen.height;
-    timer = 0;
-    black = '&blk34';       // ▓
-    grey = '&blk12;';       //  ▒
-    white = '&blk14;';      //   ░
-    bckg = {}; 
-}
--- home/access.js
+++
@@ -1,9 +0,0 @@
-
-function checkPassword() {
-	var password = document.getElementById("password").value;
-	if (password === "#cook1es!") {
-		document.getElementById("popup").style.display = "none";
-	} else {
-		alert("Incorrect password, please try again.");
-	}
-}
